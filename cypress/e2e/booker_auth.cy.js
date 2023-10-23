b9ec5c1f95f054273261437532ddad7702a8cfbb
Miguel <mctesterwork@gmail.com>
2023-10-23 10:26:41 -0500

    Added a test with an expect

--- cypress/e2e/booker_auth.cy.js
+++ cypress/e2e/booker_auth.cy.js
@@ -1,5 +1,15 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+// TODO: Change this to a command, use before each to authenticate on other tests
+describe('Authenticate to RESTful booker app', () => {
+  it('Makes a post request and receives a token', () => {
+    cy.fixture('credentials.json').then((credentials) => {
+      cy.request('POST', 'https://restful-booker.herokuapp.com/auth', {
+        username: credentials.username,
+        password: credentials.password
+      }).then((response) => {
+        expect(response.status).to.equal(200)
+        expect(response.body).to.have.property('token')
+        // TODO: Save the token to a JSON file under fixtures
+      })
+    })
   })
 })
