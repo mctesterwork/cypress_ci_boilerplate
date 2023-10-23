b327d4685e0455417347abbb215db28cfe4912a0
Miguel <mctesterwork@gmail.com>
2023-10-23 14:44:40 -0500

    more booking tests

--- cypress/e2e/booker_gets.cy.js
+++ cypress/e2e/booker_gets.cy.js
@@ -12,10 +12,20 @@ describe('Listing the bookings', () => {
         })
     })
 
-    it('Gets a specific booking ID', () => {
+    it('Gets a specific booking using ID', () => {
         cy.request('GET', '/booking/1', {auth: { 'bearer': token }}).then((response) => {
             expect(response.status).to.equal(200)
-            expect(response.body).to.have.property('firstname', 'Mark')
+            expect(response.body).to.have.property('firstname')
+        })
+    })
+
+    it('Gets a specific booking using first and last name', () => {
+        const firstname = 'Jim'
+        const lastname = 'Wilson'
+        cy.request('GET', `/booking?firstname=${firstname}&lastname=${lastname}`, {auth: { 'bearer': token }}).then((response) => {
+            console.log(response)
+            expect(response.status).to.equal(200)
+            expect(response.body[0]).to.have.property('bookingid')
         })
     })
 })
